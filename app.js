996c1bb0aebce6488da4094c839436c198f6eb46
Tristan Garside <tristan.garside@gmail.com>
2016-10-26 14:49:28 +1300

    add main function

--- app.js
+++ app.js
@@ -1,49 +1,48 @@
-var reports_url = 'http://localhost:8080/report';
-//applicationn data
-var ids = {};
+var main = function() {
 
-//functions
-var getAllReports;
-var addSingleReport;
-var deleteSpecifiedReports;
+  var reports_url = 'http://localhost:8080/report';
+  //applicationn data
+  var ids = {};
 
-$(document).ready(function () {
+  //functions
+  var getAllReports;
+  var addSingleReport;
+  var deleteSpecifiedReports;
 
   $.getJSON(reports_url, function(json) {
-    getAllReports(json);
+      getAllReports(json);
   });
-  });
 
-/** Function definitions **************************************************************/
+  /** Function definitions **************************************************************/
 
-getAllReports = function(json) {
-  var tr;
-      for (var i = 0; i < json.length; i++) {
-          tr = $('<tr>');
-          tr.append("<td class='displayed_table_element'>" + json[i].longitude + "</td>");
-          tr.append("<td class='displayed_table_element'>" + json[i].latitude + "</td>");
-          tr.append("<td class='displayed_table_element'>" + json[i].timestamp + "</td>");
-    if (json[i].altitude) {
-      tr.append("<td class='displayed_table_element'>" + json[i].altitude + "</td>" );
-    } else {
-      tr.append("<td class='displayed_table_element'>-</td>" );
-    }
-    if (json[i].accuracy) {
-      tr.append("<td class='displayed_table_element'>" + json[i].accuracy + "</td>" );
-    } else {
-      tr.append("<td class='displayed_table_element'>-</td>" );
-    }
-    tr.append("<td class='displayed_table_element'>" + json[i]._id + "</td>");
-    //add id to application state
-
-    tr.append("<td class='displayed_table_element'><input type='checkbox' id='cb_" + json[i]._id + "'/></td>");
-          tr.append("</tr>");
-    $('#data_table').append(tr);
+  getAllReports = function(json) {
+    var tr;
+        for (var i = 0; i < json.length; i++) {
+            tr = $('<tr>');
+            tr.append("<td class='displayed_table_element'>" + json[i].longitude + "</td>");
+            tr.append("<td class='displayed_table_element'>" + json[i].latitude + "</td>");
+            tr.append("<td class='displayed_table_element'>" + json[i].timestamp + "</td>");
+      if (json[i].altitude) {
+        tr.append("<td class='displayed_table_element'>" + json[i].altitude + "</td>" );
+      } else {
+        tr.append("<td class='displayed_table_element'>-</td>" );
       }
-  $('#data_table').append("<tr><td></td><td></td><td></td><td></td><td></td><td></td>" +
-              "<td><input type='button' value='delete selected' onClick='deleteSpecifiedReports()'/></td>");
+      if (json[i].accuracy) {
+        tr.append("<td class='displayed_table_element'>" + json[i].accuracy + "</td>" );
+      } else {
+        tr.append("<td class='displayed_table_element'>-</td>" );
+      }
+      tr.append("<td class='displayed_table_element'>" + json[i]._id + "</td>");
+      //add id to application state
 
-}
+      tr.append("<td class='displayed_table_element'><input type='checkbox' id='cb_" + json[i]._id + "'/></td>");
+            tr.append("</tr>");
+      $('#data_table').append(tr);
+        }
+    $('#data_table').append("<tr><td></td><td></td><td></td><td></td><td></td><td></td>" +
+                "<td><input type='button' value='delete selected' onClick='deleteSpecifiedReports()'/></td>");
+
+  }
 
 addSingleReport = function() {
   console.log('addSingReport() invoked');
@@ -64,14 +63,17 @@ addSingleReport = function() {
   $.post(reports_url, data, 'json');
 };
 
-deleteSpecifiedReports = function() {
-  var rowCount = $('#data_table tr').length - 1 //One row is for delete button itself
-  console.log(document.getElementById(data_table).rows[0]);
-  var toDelete = [];
-  console.log(rowCount);
-  for(var i=0; i<rowCount; i++) {
+  deleteSpecifiedReports = function() {
+    var rowCount = $('#data_table tr').length - 1 //One row is for delete button itself
+    console.log(document.getElementById(data_table).rows[0]);
+    var toDelete = [];
+    console.log(rowCount);
+    for(var i=0; i<rowCount; i++) {
 
-    //var extractedID = $('#)
-    //if ()
-  }
+      //var extractedID = $('#)
+      //if ()
+    }
+  };
 };
+
+$(document).ready(main);
